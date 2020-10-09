e1587cba159157b6547e740a36a31bac9cba7d1a
Emmanuel Padilla <manolin923@gmail.com>
2020-10-09 08:19:41 -0700

    initial commit, index.js modified

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { featureReducer } from "./reducers/reducer";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(smurfReducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
